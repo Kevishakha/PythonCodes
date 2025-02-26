f8f363edc6ae67f968dbcf7d3ad07616c520cd9e
vishakha <kevishakha@gmail.com>
2025-02-26 13:34:34 +0530

    Create Day1.py

---
+++ UnifiedMentor/Day1.py
@@ -0,0 +1,28 @@
+print('vishakha')
+
+name="Rishika"
+age=20
+student=['RIshika', 20, 90.2]  # list mutable
+students = ('RIshika', 20, 90.2) # tuple immutable
+range(5)
+print(range(5))
+type(student)
+type(students)
+
+student = {'Name':'Rishika','Age':20,'Marks':90.2}
+print(student)
+student.keys()
+student.values()
+print(type(student))
+
+# Set
+
+students= {'Rishika', 'Rishika', 20, 90.2}
+print(students)
+print(type(students))
+
+# boolean
+bool(1)
+bool(0)
+bool(-1)
+bool(5)
