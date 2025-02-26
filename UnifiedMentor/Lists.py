34fcc1cb03b47e04f3cb1f71f41bfefc7c62a036
vishakha <kevishakha@gmail.com>
2025-02-26 13:38:54 +0530

    Create Lists.py
    [25, 45, 85.85, 25, 6, 4, 65, 5, 1, 51, 5, 55, 5, 55, 215, 54, 1, 4, 1, 554, 1, 15, 5, 51, 522]
    [1, 1, 1, 1, 4, 4, 5, 5, 5, 5, 6, 15, 25, 25, 45, 51, 51, 54, 55, 55, 65, 85.85, 215, 522, 554]
    [554, 522, 215, 85.85, 65, 55, 55, 54, 51, 51, 45, 25, 25, 15, 6, 5, 5, 5, 5, 4, 4, 1, 1, 1, 1]
    11
    4
    [554, 522, 215, 85.85, 65, 55, 55, 54, 51, 51, 45, 25, 25, 15, 6, 5, 5, 5, 5, 4, 4, 1, 1, 1, 1, 'vish', 'ixmxm', 'Visnka']
    []

---
+++ UnifiedMentor/Lists.py
@@ -0,0 +1,30 @@
+# List and Its methods
+# append- add at last
+# insert(index, value)
+# pop(index)
+# remove(element)
+# reverse
+# sort
+# index gives the length
+# count
+# extend
+# copy
+# clear
+
+print("Try programiz.pro")
+mylist=[25,45,85.85,25,6,4,65,5,1,51,5,55,5,55,215,54,1,4,1,554,1,15,5,51]
+print(type(mylist))
+mylist.append(522)
+print(mylist)
+mylist.sort()
+print(mylist)
+mylist.reverse()
+print(mylist)
+print(mylist.index(25))
+print(mylist.count(5))
+list=['vish','ixmxm','Visnka']
+mylist.extend(list)
+print(mylist)
+mylist.copy()
+mylist.clear()
+print(mylist)
