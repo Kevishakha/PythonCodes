b87b660fe576a0e9defd1a9fa4d7008ac1ac96d8
vishakha <kevishakha@gmail.com>
2025-02-26 13:35:49 +0530

    Create Operators.py

---
+++ UnifiedMentor/Operators.py
@@ -0,0 +1,16 @@
+# Python operators
+# Arithmetic(+,-,,/,%,*),
+# Assignment(=,+=,-=,=,/=,%=,*=),
+# Comparision(==,!=,<,>,<=,>=),
+# logical(x and y , x or y, not y)
+# Membership operators used in list tuple (in , not in)k
+# List item
+
+x=5
+y=8
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
