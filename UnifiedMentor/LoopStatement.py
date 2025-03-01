8a84d5ca6b0579f20588ca50d67a50b4e1fa4aee
vishakha <kevishakha@gmail.com>
2025-03-01 20:52:53 +0530

    Create LoopStatement.py

---
+++ UnifiedMentor/LoopStatement.py
@@ -0,0 +1,25 @@
+# Python loop Statements
+
+# If statement
+# if elif else statement
+# while
+# if statement
+
+
+# if statement
+a=20;
+b=30
+
+if b>a:
+  print("b is greater than a")
+
+
+# if elif statement
+
+aman,vinod=80,80
+if vinod>aman:
+  print("vinod is greater than aman")
+elif aman>vinod:
+  print("aman is greater than vinod")
+else:
+  print("both are equal")
