3d70f6a9c8f3a554b17f5e0ab0792cbd24038d91
vishakha <kevishakha@gmail.com>
2025-03-11 19:23:14 +0530

    Create tuple.py

---
+++ UnifiedMentor/tuple.py
@@ -0,0 +1,15 @@
+print("Try programiz.pro") 
+#  tuple are used to store a various data structure in single var
+# A tuple is ac collection is ordered and unchangable . It is written in round brackets
+# tuple are immutable
+# max()
+# index 
+# tuple()
+# count()
+# min()
+# allow duplicates no uniqueness
+
+fruits=('apple','cherry','banane','apple','apple','apple' )
+print(fruits)
+print(fruits.count('apple'))
+print(fruits.index('banane'))
